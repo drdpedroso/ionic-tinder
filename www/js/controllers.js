86064a4b97dcfb891d1a54b3a73cd499f1fb4de1
Eduardo Pedroso <drdpedroso@gmail.com>
2016-09-08 16:44:08 -0300

    Adicionando informacoes aos cards (idade + nome)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('controllers', [])
   .controller('HomeController', function($scope){
     $scope.persons = [
-      {'photo' : 'http://s2.glbimg.com/pXH2qoCQFOGFm6VmCJZvvL68m98=/e.glbimg.com/og/ed/f/original/2016/06/14/untitled-3_1.jpg', 'name' : 'Lilian', 'idade': '21'},
-      {'photo' : 'http://s2.glbimg.com/pXH2qoCQFOGFm6VmCJZvvL68m98=/e.glbimg.com/og/ed/f/original/2016/06/14/untitled-3_1.jpg', 'name' : 'Larissa', 'idade' : '18'}
+      {'photo' : 'http://s2.glbimg.com/pXH2qoCQFOGFm6VmCJZvvL68m98=/e.glbimg.com/og/ed/f/original/2016/06/14/untitled-3_1.jpg', 'name' : 'Lilian', 'age': '21'},
+      {'photo' : 'http://s2.glbimg.com/pXH2qoCQFOGFm6VmCJZvvL68m98=/e.glbimg.com/og/ed/f/original/2016/06/14/untitled-3_1.jpg', 'name' : 'Larissa', 'age' : '18'}
     ]
   })
