69416d8e9a6b1d29833af5fb63b26a8ed0012f32
Eduardo Pedroso <drdpedroso@gmail.com>
2016-09-08 23:02:13 -0300

    Colocando ação botao não

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,4 +4,9 @@ angular.module('controllers', [])
       {'photo' : 'http://s2.glbimg.com/pXH2qoCQFOGFm6VmCJZvvL68m98=/e.glbimg.com/og/ed/f/original/2016/06/14/untitled-3_1.jpg', 'name' : 'Lilian', 'age': '21'},
       {'photo' : 'http://s2.glbimg.com/eSWSdBeYTNdjxJYSX3YQ5IVm12A=/s.glbimg.com/et/gs/f/original/2015/10/23/gabee2.jpg', 'name' : 'Larissa', 'age' : '18'}
     ]
+
+    $scope.swipeCard = function(person){
+      $scope.persons.shift(person);
+    }
+
   })
